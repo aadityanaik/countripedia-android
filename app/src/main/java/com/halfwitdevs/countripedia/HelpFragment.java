6cdb7132c1faa6f60989f32211d17ebe6abc9d7f
thealphaversion <alphabuildv1@gmail.com>
2018-06-26 02:33:25 +0530

    finished help

--- app/src/main/java/com/halfwitdevs/countripedia/HelpFragment.java
+++ app/src/main/java/com/halfwitdevs/countripedia/HelpFragment.java
@@ -7,17 +7,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HelpFragment extends Fragment{
 
     View view;
+    HelpDataAdapter helpDataAdapter;
+    ExpandableListView expandableListView;
+    List<String> listGroupHeader;
+    HashMap<String, List<String>> listChildData;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_help_layout, container, false);
+
+        expandableListView = (ExpandableListView)view.findViewById(R.id.help_list);
+        prepareList();
+        helpDataAdapter = new HelpDataAdapter(getActivity(), listGroupHeader, listChildData);
+        expandableListView.setAdapter(helpDataAdapter);
         return view;
     }
-}
 
-//Left blank intentionally because there's no helping help
+    public void prepareList(){
+        listGroupHeader = new ArrayList<String>();
+        listChildData = new HashMap<String, List<String>>();
+
+        listGroupHeader.add("First Look");
+        listGroupHeader.add("Selecting countries");
+
+        List<String> firstLookData = new ArrayList<String>();
+        firstLookData.add("Welcome");
+
+        List<String> selectingCountriesData = new ArrayList<String>();
+        selectingCountriesData.add("Country");
+
+        listChildData.put(listGroupHeader.get(0), firstLookData);
+        listChildData.put(listGroupHeader.get(1), selectingCountriesData);
+    }
+}
